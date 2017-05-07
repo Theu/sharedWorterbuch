2563de80d521f56bf87ebe873b5d394e408f063e
Theu <info@matteocontini.com>
2017-05-07 14:33:01 +0200

    working on props and state

--- SHWB-REACT/src/main.js
+++ SHWB-REACT/src/main.js
@@ -1,11 +1,46 @@
-console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
- 
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
-});
+
+// function Clock(props) {
+//   return (
+//     <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//   );
+// } LIKE TO SAY:
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div>Hello, World!</div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
+
+// setInterval(tick, 1000)
